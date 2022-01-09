90230c6dbfcae79914b05c4f65bd0e396fa8cd24
payalkherajani <payal.kherajani19@gmail.com>
2022-01-09 16:08:40 +0530

    schema-created-backend

---
+++ backend/graphql/schema/index.js
@@ -0,0 +1,31 @@
+const { buildSchema } = graphql
+
+const schema = buildSchema(`
+ type TodoItem{
+     _id: ID!
+     text: String!
+     isCompleted: Boolean!
+     createdAt: String!
+ }
+
+ input TodoItemInput{
+     text: String!
+     isCompleted: Boolean!
+ }
+
+ type Query{
+     todos: [TodoItem!]
+ }
+
+ type Mutation{
+     addTodoItem(todoDetails:TodoItemInput): TodoItem
+ }
+
+ schema{
+     query: Query
+     mutation: Mutation
+ }
+`)
+
+
+module.exports = schema
